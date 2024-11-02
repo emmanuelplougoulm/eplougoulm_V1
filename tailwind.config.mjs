3f7ee529e7b489b2bf3d7a1de384b557ae8a51b4
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-11-02 15:33:58 +0100

    [EP]: add custom tailwind colors + update views

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
-			colors:{
-				'regular':'green',
-				'highlighted':'red'
-			}
+			colors: {
+				"purple-dark": "#151526",
+				"purple-electric": "#8C64C4",
+				"grey-light": "#fdfdfc",
+				"grey-medium": "#b2b7cf",
+
+				divider: "#545454",
+			},
 		},
 	},
 	plugins: [],
-}
+};
