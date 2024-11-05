f5d157e012b84c26cc000840753174ba6e0201c5
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-11-05 18:00:23 +0100

    [EP]: add archive config

---
+++ src/config/projects-archive.config.ts
@@ -0,0 +1,51 @@
+interface Project {
+	year: string;
+	project: string;
+	href: string;
+	tags: string[];
+}
+
+export const projectArchive: Array<Project> = [
+	{
+		year: "2024",
+		project: "Cryptobubbles clone",
+		tags: ["Vite.js", "Biome.js", "Shadcn", "React.js", "Bun"],
+		href: "https://github.com/emmanuelplougoulm/cryptobubbles-clone",
+	},
+	{
+		year: "2024",
+		project: "P4 Collective webapp",
+		tags: ["Next.js", "Semantic Html", "SSR/CSR", "Web performance"],
+		href: "https://nextjs-collectif-p4.vercel.app/",
+	},
+	{
+		year: "2023",
+		project: "Moula manager",
+		tags: ["Vue.js", "Nest.js", "Typescript"],
+		href: "https://github.com/emmanuelplougoulm/moula-manager",
+	},
+	{
+		year: "2024",
+		project: "Worklife",
+		tags: ["Vue.js", "Nest.js", "Typescript"],
+		href: "https://github.com/emmanuelplougoulm/test_worklife",
+	},
+	{
+		year: "2024",
+		project: "Weglot",
+		tags: ["Javascript"],
+		href: "https://github.com/emmanuelplougoulm/weglot_test",
+	},
+	{
+		year: "2024",
+		project: "First Dapp",
+		tags: [""],
+		href: "https://github.com/emmanuelplougoulm/dapp",
+	},
+	{
+		year: "2021",
+		project: "Automating copass",
+		tags: [""],
+		href: "https://github.com/emmanuelplougoulm/automating-copass",
+	},
+];
