19b3cf7b7d627dc6ea5053dae2d8f77a32429d4d
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-08 21:01:28 +0200

    [EP]: add new project card

--- src/config/projects.config.ts
+++ src/config/projects.config.ts
@@ -1,37 +1,45 @@
 interface Project {
-	title: string;
-	description: string;
-	ariaLabel: string;
-	href: string;
-	img: string;
-	tags: string[];
+  title: string;
+  description: string;
+  ariaLabel: string;
+  href: string;
+  img: string;
+  tags: string[];
 }
 
 export const projects: Array<Project> = [
-	{
-		title: "P4 Collective webapp",
-		description: "Theater collective website",
-		ariaLabel: "Build a website for a theater collective",
-		href: "https://nextjs-collectif-p4.vercel.app/",
-		img: "/p4_thumbnail.webp",
-		tags: ["Next.js", "Semantic Html", "SSR/CSR", "Web performance"],
-	},
-	{
-		title: "CryptoBubbles clone",
-		description:
-			"A clone of the famous crypto market forecast built with modern React tools",
-		ariaLabel: "Build a cryptobubbles clone (opens in a new tab)",
-		href: "https://github.com/emmanuelplougoulm/cryptobubbles-clone",
-		img: "/bubbles_thumbnail.webp",
-		tags: ["Vite.js", "Biome.js", "Shadcn", "React.js", "Bun"],
-	},
-	{
-		title: "Moula manager",
-		description:
-			"Webapp based on NuxtJS to display crypto-currencies portfolio",
-		ariaLabel: "Webapp based on NuxtJS to display crypto-currencies portfolio",
-		href: "https://github.com/emmanuelplougoulm/moula-manager",
-		img: "/moula_manager.webp",
-		tags: ["Vue.js", "Nest.js", "Typescript"],
-	},
+  {
+    title: "Component library",
+    description: "A homemade library built for my projects",
+    ariaLabel: "A homemade library built for my projects",
+    href: "https://emmanuelplougoulm.github.io/components_library/",
+    img: "",
+    tags: ["Vue.js", "Design system"],
+  },
+  {
+    title: "P4 Collective webapp",
+    description: "Theater collective website",
+    ariaLabel: "Build a website for a theater collective",
+    href: "https://nextjs-collectif-p4.vercel.app/",
+    img: "/p4_thumbnail.webp",
+    tags: ["Next.js", "Semantic Html", "SSR/CSR", "Web performance"],
+  },
+  {
+    title: "CryptoBubbles clone",
+    description:
+      "A clone of the famous crypto market forecast built with modern React tools",
+    ariaLabel: "Build a cryptobubbles clone (opens in a new tab)",
+    href: "https://github.com/emmanuelplougoulm/cryptobubbles-clone",
+    img: "/bubbles_thumbnail.webp",
+    tags: ["Vite.js", "Biome.js", "Shadcn", "React.js", "Bun"],
+  },
+  {
+    title: "Moula manager",
+    description:
+      "Webapp based on NuxtJS to display crypto-currencies portfolio",
+    ariaLabel: "Webapp based on NuxtJS to display crypto-currencies portfolio",
+    href: "https://github.com/emmanuelplougoulm/moula-manager",
+    img: "/moula_manager.webp",
+    tags: ["Vue.js", "Nest.js", "Typescript"],
+  },
 ];
