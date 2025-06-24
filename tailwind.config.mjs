22e7be7a0fe9525308cfd3d227e4789bacb3141c
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-06-24 13:53:12 +0200

    update infos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			colors: {
-				"color-primary": "#66ffff" /* Light purple */,
-				"color-secondary": " #ccd6f6" /* Blanc cassé */,
-				"color-tertiary": " #8892b0" /* Gris clair */,
-				"color-background": "#151526" /* Dark purple */,
-				"color-accent": " #64ffda" /* Same primary color, used to accentuate */,
-			},
-			blur: {
-				"4xl": "64px",
-				"5xl": "80px",
-				"6xl": "100px",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#FFFDFD" /* Light purple */,
+        "color-secondary": " #ffffff" /* Blanc cassé */,
+        "color-tertiary": " #FFFDFD" /* Gris clair */,
+        "color-background": "#272F96" /* Dark purple */,
+        "color-accent": " #64ffda" /* Same primary color, used to accentuate */,
+      },
+      blur: {
+        "4xl": "64px",
+        "5xl": "80px",
+        "6xl": "100px",
+      },
+    },
+  },
+  plugins: [],
 };
